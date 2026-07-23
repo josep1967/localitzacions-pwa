cb449b70275015381d22219d92f25c01232d48ff
josep1967 <pc.enginyeria@gmail.com>
2026-07-23 21:45:53 +0200

    mod 1

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', () => {
+  console.log('Service Worker instal·lat');
+});
+
+self.addEventListener('activate', () => {
+  console.log('Service Worker activat');
+});
